bdfd2e22238fdb716ca80caf7b58959f9c9496da
Girish Patil <girishpatil3002@gmail.com>
2023-06-19 19:01:20 +0530

    Modified

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,39 +1,38 @@
-import React from 'react'
-import "./Hero.css"
+import React from "react";
+import "./Hero.css";
 import TypeWriter from "typewriter-effect";
 
-const Hero = ({active}) => {
-    return (
-        <div>
-
-            <div className="Hero">
-               
-                   
-                    {active &&<div className="type-writer">
-                        <div className="static-text">
-                            Digambara Digambara
-                        </div>
-                        <div className="typewriter-text">
-                        {<TypeWriter 
-                            options={{
-                                strings: ["Shreepad Vallabh Digambara.", "Datta Gurunche Naam Smara.", "Bholi Seva Manya Kara."],
-                                autoStart: true,
-                                loop: true,
-                                cursorClassName: "cursor"
-                            }} />}
-                        </div>
-                      
-
-                    </div>}
-                    {/* <button onClick={() => navigate('/category')} className="cta btn-primary">Explore more</button> */}
-                
+const Hero = ({ active }) => {
+  return (
+    <div>
+      <div className="Hero">
+        {active && (
+          <div className="type-writer">
+            {/* <div className="static-text">Digambara Digambara</div> */}
+            <div className="static-text"> दिगंबरा दिगंबरा</div>
+            <div className="typewriter-text">
+              {
+                <TypeWriter
+                  options={{
+                    // strings: ["Shreepad Vallabh Digambara.", "Datta Gurunche Naam Smara.", "Bholi Seva Manya Kara."],
+                    strings: [
+                      "श्रीपाद वल्लभ दिगंबरा ",
+                      "दत्त गुरुंचे नाम स्मरा ",
+                      "भोळी सेवा मान्य करा ",
+                    ],
+                    autoStart: true,
+                    loop: true,
+                    cursorClassName: "cursor",
+                  }}
+                />
+              }
             </div>
+          </div>
+        )}
+        {/* <button onClick={() => navigate('/category')} className="cta btn-primary">Explore more</button> */}
+      </div>
+    </div>
+  );
+};
 
-
-
-
-        </div>
-    )
-}
-
-export default Hero
+export default Hero;
